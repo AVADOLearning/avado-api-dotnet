5175b84e2d2868523474a99ce939c3da953d520f
Earle Skinner <Earle.Skinner@floream.com>
2015-10-13 09:56:15 +0100

    Removed reference to a security dll and created a local version

---
+++ ApiDemo/ApiDemo/ClientMessageInspector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using ApiDemo;
+
+namespace BusinessLogicService.Security // Do not change this
+{
+    /// <summary>
+    /// Sample code to attach credentials to the service message.
+    /// </summary>
+    public class ClientMessageInspector : BehaviorExtensionElement, IClientMessageInspector, IEndpointBehavior
+    {
+        #region [ Public Members ]
+
+        public Guid AuthToken { get; set; }
+        
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            var lToken = new Token { AuthToken = AuthToken };
+            request.Headers.Add(lToken.ToMessageHeader());
+            return null;
+        }
+
+        public override Type BehaviorType
+        {
+            get { return GetType(); }
+        }
+
+        protected override object CreateBehavior()
+        {
+            return this;
+        }
+
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.MessageInspectors.Add(this);
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ ApiDemo/ApiDemo/Token.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel.Channels;
+
+namespace ApiDemo
+{
+    [DataContract]
+    public sealed class Token
+    {
+        public const string CREDENTIALS_HEADER = "Authenticate";
+        public const string CREDENTIALS_NAMESPACE = "";
+        public const string APIKEY_HEADER = "apikey";
+
+        [DataMember]
+        public Guid AuthToken { get; set; }
+
+        public MessageHeader ToMessageHeader()
+        {
+            var lHeader = MessageHeader.CreateHeader(CREDENTIALS_HEADER, CREDENTIALS_NAMESPACE, this);
+            return lHeader;
+        }
+    }
+}
